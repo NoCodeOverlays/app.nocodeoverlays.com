c27f3b20ad59990b02ad5ee60cf5147dc960c26a
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-29 19:16:24 -0400

    Updating icon sizes and fixing classnames

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -1,17 +1,15 @@
-import classNames from "classnames/bind";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import styles from "./Icon.module.scss";
-
-let cx = classNames.bind(styles);
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Icon.module.scss';
 
 const Icon = ({ name, small, large }) => {
-  let className = cx({
-    "Icon--small": small,
-    "Icon--large": large,
+  let cx = classNames.bind(styles);
+  let className = cx('Icon', {
+    'Icon--small': !!small,
+    'Icon--large': !!large,
   });
 
-  return <FontAwesomeIcon className={className} icon={name || "smile"} />;
+  return <FontAwesomeIcon className={className} icon={name || 'smile'} />;
 };
 
 export default Icon;
