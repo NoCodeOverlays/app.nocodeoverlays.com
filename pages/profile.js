ab8b8fd0ed6d00b630024fa29a6574ccbe734669
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-30 15:26:14 -0400

    Making profile page work better with user methods

--- pages/profile.js
+++ pages/profile.js
@@ -5,107 +5,147 @@ import { Button, Input } from '@amb-codes-crafts/a11y-components';
 import { Layout } from '../components';
 
 const ProfilePage = () => {
-  const router = useRouter();
   const { user, userLoading } = useAuth();
+  const [userObject, setUserObject] = useState(user || {});
+  const [displayName, setDisplayName] = useState(userObject.displayName || '');
+  const [email, setEmail] = useState(userObject.email || '');
+  const [newPassword, setNewPassword] = useState('');
 
-  useEffect(() => {
-    if (!userLoading && !user) {
-      router.replace('/login');
+  const getContent = () => {
+    if (userLoading) {
+      return <h1>Loading...</h1>;
     }
-  }, [userLoading, user]);
 
-  if (userLoading) {
-    return <h1>Loading...</h1>;
-  }
+    if (!user) {
+      return <h1>Redirecting</h1>;
+    }
 
-  if (!user) {
-    return <h1>Redirecting</h1>;
-  }
+    return (
+      <>
+        <h2>Profile Page</h2>
+        <p>Member since: {userObject.metadata.creationTime}</p>
+        <br />
 
-  const [displayName, setDisplayName] = useState(user.displayName || '');
-  const [email, setEmail] = useState(user.email || '');
-
-  return (
-    <Layout title="No-Code Overlays | Profile">
-      <h2>Profile Page</h2>
-      <p>Member since: {user.metadata.creationTime}</p>
-      <br />
-
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <div style={{ flexGrow: 1, marginRight: 24 }}>
-          <Input
-            label="Display name"
-            type="text"
-            value={displayName}
-            onChange={(e) => {
-              setDisplayName(e.target.value);
+        <div style={{ maxWidth: 600 }}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
             }}
-          />
-        </div>
-        <Button
-          disabled={!(!!displayName && displayName !== user.displayName)}
-          onClick={() => {
-            user.updateProfile({ displayName }).then(() => {
-              alert('New display name saved.');
-            });
-          }}
-        >
-          Save
-        </Button>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <div style={{ flexGrow: 1, marginRight: 24 }}>
-          <Input
-            label="Email address"
-            type="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </div>
-        {!!email && email !== user.email ? (
-          <Button
-            onClick={() => {
-              user
-                .verifyBeforeUpdateEmail(email)
-                .then(() => {
-                  alert('Verification email sent. Please check your inbox.');
-                })
-                .catch((error) => {
-                  alert(error.message);
+          >
+            <div style={{ flexGrow: 1, marginRight: 24 }}>
+              <Input
+                label="Display name"
+                type="text"
+                value={displayName}
+                onChange={(e) => {
+                  setDisplayName(e.target.value);
+                }}
+              />
+            </div>
+            <Button
+              disabled={
+                !(!!displayName && displayName !== userObject.displayName)
+              }
+              onClick={() => {
+                user.updateProfile({ displayName }).then(() => {
+                  alert('New display name saved.');
+                  setUserObject({ ...userObject, displayName });
                 });
+              }}
+            >
+              Save
+            </Button>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
             }}
           >
-            Update Email
-          </Button>
-        ) : (
-          <Button
-            disabled={user.emailVerified}
-            onClick={() => {
-              user.sendEmailVerification().then(() => {
-                alert('Email verification sent. Please check your inbox.');
-              });
+            <div style={{ flexGrow: 1, marginRight: 24 }}>
+              <Input
+                label="Email address"
+                type="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            {!!email && email !== userObject.email ? (
+              <Button
+                onClick={() => {
+                  user
+                    .verifyBeforeUpdateEmail(email)
+                    .then(() => {
+                      alert(
+                        'Verification email sent. Please check your inbox.',
+                      );
+                      setUserObject({ ...userObject, email });
+                    })
+                    .catch((error) => {
+                      alert(error.message);
+                    });
+                }}
+              >
+                Update Email
+              </Button>
+            ) : (
+              <Button
+                disabled={user.emailVerified}
+                onClick={() => {
+                  user.sendEmailVerification().then(() => {
+                    alert('Email verification sent. Please check your inbox.');
+                  });
+                }}
+              >
+                {user.emailVerified ? 'Verified' : 'Verify'}
+              </Button>
+            )}
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
             }}
           >
-            {user.emailVerified ? 'Verified' : 'Verify'}
-          </Button>
-        )}
-      </div>
-    </Layout>
-  );
+            <div style={{ flexGrow: 1, marginRight: 24 }}>
+              <Input
+                label="Password"
+                type="password"
+                value={newPassword}
+                onChange={(e) => {
+                  setNewPassword(e.target.value);
+                }}
+              />
+            </div>
+            <Button
+              disabled={!newPassword}
+              onClick={() => {
+                user
+                  .updatePassword(newPassword)
+                  .then(() => {
+                    alert('New password saved.');
+                    setNewPassword('');
+                  })
+                  .catch((err) => {
+                    alert(err.message);
+                  });
+              }}
+            >
+              Save
+            </Button>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return <Layout title="No-Code Overlays | Profile">{getContent()}</Layout>;
 };
 
 export default ProfilePage;
