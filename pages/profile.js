67182259402712005702a41b442718b51624f8b6
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-10-29 23:50:20 -0400

    Adding profile page with display name and email

---
+++ pages/profile.js
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '../context/auth';
+import { Button, Input } from '@amb-codes-crafts/a11y-components';
+import { Layout } from '../components';
+
+const ProfilePage = () => {
+  const router = useRouter();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!user) {
+    router.replace('/login');
+    return;
+  }
+
+  const [displayName, setDisplayName] = useState(user.displayName || '');
+  const [email, setEmail] = useState(user.email || '');
+
+  return (
+    <Layout title="Profile">
+      <h2>Profile Page</h2>
+      <p>Member since: {user.metadata.creationTime}</p>
+      <br />
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ flexGrow: 1, marginRight: 24 }}>
+          <Input
+            label="Display name"
+            type="text"
+            value={displayName}
+            onChange={(e) => {
+              setDisplayName(e.target.value);
+            }}
+          />
+        </div>
+        <Button
+          disabled={!(!!displayName && displayName !== user.displayName)}
+          onClick={() => {
+            user.updateProfile({ displayName }).then(() => {
+              alert('New display name saved.');
+            });
+          }}
+        >
+          Save
+        </Button>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ flexGrow: 1, marginRight: 24 }}>
+          <Input
+            label="Email address"
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        {!!email && email !== user.email ? (
+          <Button
+            onClick={() => {
+              user
+                .verifyBeforeUpdateEmail(email)
+                .then(() => {
+                  alert('Verification email sent. Please check your inbox.');
+                })
+                .catch((error) => {
+                  alert(error.message);
+                });
+            }}
+          >
+            Update Email
+          </Button>
+        ) : (
+          <Button
+            disabled={user.emailVerified}
+            onClick={() => {
+              user.sendEmailVerification().then(() => {
+                alert('Email verification sent. Please check your inbox.');
+              });
+            }}
+          >
+            {user.emailVerified ? 'Verified' : 'Verify'}
+          </Button>
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default ProfilePage;
