ffcda426b90b4260f4eb7770da1640da0041e35d
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2020-11-07 11:32:22 -0500

    Putting better default data in useOverlay

--- context/overlay.js
+++ context/overlay.js
@@ -2,17 +2,23 @@ import { createContext, useState, useContext, useEffect } from 'react';
 import { useAuth } from './auth';
 import { firebaseAPI } from '../lib/firebase';
 
-const OverlayContext = createContext({ data: null, dataLoading: null });
+const DEFAULT_DATA = { width: '', height: '', widgets: {} };
+const DEFAULT_DATA_LOADING = false;
+
+const OverlayContext = createContext({
+  data: DEFAULT_DATA,
+  dataLoading: DEFAULT_DATA_LOADING,
+});
 
 export const OverlayProvider = ({ children }) => {
   const { user, userLoading } = useAuth();
-  const [dataLoading, setDataLoading] = useState();
-  const [data, setData] = useState();
+  const [dataLoading, setDataLoading] = useState(DEFAULT_DATA_LOADING);
+  const [data, setData] = useState(DEFAULT_DATA);
 
   useEffect(() => {
     setDataLoading(true);
     return firebaseAPI('getOverlayData').then((res) => {
-      setData(res);
+      setData({ ...DEFAULT_DATA, ...res });
       setDataLoading(false);
     });
   }, [user, userLoading]);
