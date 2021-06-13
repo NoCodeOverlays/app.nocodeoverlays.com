4d901f92d5ba0d54feb8ba98b143746285fb16d0
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-06-13 14:41:04 -0400

    Rendering widgets by position

--- components/EditOverlaySidebar/components/WidgetList.jsx
+++ components/EditOverlaySidebar/components/WidgetList.jsx
@@ -2,22 +2,24 @@ import { firebaseAPI } from '../../../lib/firebase';
 import Widget from './Widget';
 
 const WidgetList = ({ setIsSaving, setWidgets, widgets }) =>
-  Object.keys(widgets).map((widgetKey) => {
-    const widget = widgets[widgetKey];
-    return (
-      <Widget
-        deleteWidget={() => {
-          delete widgets[widgetKey];
-          setIsSaving(true);
-          firebaseAPI('updateOverlayWidgets', widgets).then(() => {
-            setWidgets({ ...widgets });
-            setIsSaving(false);
-          });
-        }}
-        widget={widget}
-        widgetKey={widgetKey}
-      />
-    );
-  });
+  Object.keys(widgets)
+    .sort((a, b) => (a.position < b.position ? -1 : 1))
+    .map((widgetKey) => {
+      const widget = widgets[widgetKey];
+      return (
+        <Widget
+          deleteWidget={() => {
+            delete widgets[widgetKey];
+            setIsSaving(true);
+            firebaseAPI('updateOverlayWidgets', widgets).then(() => {
+              setWidgets({ ...widgets });
+              setIsSaving(false);
+            });
+          }}
+          widget={widget}
+          widgetKey={widgetKey}
+        />
+      );
+    });
 
 export default WidgetList;
--- components/Overlay/Overlay.jsx
+++ components/Overlay/Overlay.jsx
@@ -8,59 +8,61 @@ const Overlay = ({ width, height, widgets }) => (
       backgroundColor: 'white',
     }}
   >
-    {Object.keys(widgets).map((widgetKey, index) => {
-      const widget = widgets[widgetKey];
-      if (widget.type === 'color') {
-        return (
-          <div
-            key={`widget-${index}`}
-            style={{
-              width: `${widget.width}px`,
-              height: `${widget.height}px`,
-              backgroundColor: widget.color,
-              position: 'absolute',
-              top: `${widget.yPosition}px`,
-              left: `${widget.xPosition}px`,
-            }}
-          ></div>
-        );
-      } else if (widget.type === 'image') {
-        return (
-          <img
-            key={`widget-${index}`}
-            src={widget.url}
-            style={{
-              width: `${widget.width}px`,
-              height: `${widget.height}px`,
-              position: 'absolute',
-              top: `${widget.yPosition}px`,
-              left: `${widget.xPosition}px`,
-            }}
-          />
-        );
-      } else if (widget.type === 'text') {
-        return (
-          <span
-            key={`widget-${index}`}
-            style={{
-              width: `${widget.width}px`,
-              height: `${widget.height}px`,
-              lineHeight: `${widget.height}px`,
-              position: 'absolute',
-              top: `${widget.yPosition}px`,
-              left: `${widget.xPosition}px`,
-              fontFamily: widget.fontFamily,
-              fontSize: `${widget.fontSize}px`,
-              color: `${widget.color}`,
-              textAlign: `${widget.textAlign}`,
-              verticalAlign: `${widget.verticalAlign}`,
-            }}
-          >
-            {widget.text}
-          </span>
-        );
-      }
-    })}
+    {Object.keys(widgets)
+      .sort((a, b) => (a.position < b.position ? -1 : 1))
+      .map((widgetKey, index) => {
+        const widget = widgets[widgetKey];
+        if (widget.type === 'color') {
+          return (
+            <div
+              key={`widget-${index}`}
+              style={{
+                width: `${widget.width}px`,
+                height: `${widget.height}px`,
+                backgroundColor: widget.color,
+                position: 'absolute',
+                top: `${widget.yPosition}px`,
+                left: `${widget.xPosition}px`,
+              }}
+            ></div>
+          );
+        } else if (widget.type === 'image') {
+          return (
+            <img
+              key={`widget-${index}`}
+              src={widget.url}
+              style={{
+                width: `${widget.width}px`,
+                height: `${widget.height}px`,
+                position: 'absolute',
+                top: `${widget.yPosition}px`,
+                left: `${widget.xPosition}px`,
+              }}
+            />
+          );
+        } else if (widget.type === 'text') {
+          return (
+            <span
+              key={`widget-${index}`}
+              style={{
+                width: `${widget.width}px`,
+                height: `${widget.height}px`,
+                lineHeight: `${widget.height}px`,
+                position: 'absolute',
+                top: `${widget.yPosition}px`,
+                left: `${widget.xPosition}px`,
+                fontFamily: widget.fontFamily,
+                fontSize: `${widget.fontSize}px`,
+                color: `${widget.color}`,
+                textAlign: `${widget.textAlign}`,
+                verticalAlign: `${widget.verticalAlign}`,
+              }}
+            >
+              {widget.text}
+            </span>
+          );
+        }
+      })}
   </div>
 );
 
