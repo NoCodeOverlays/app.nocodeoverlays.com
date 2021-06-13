a0af21aea52da4578a381e573155c12f3df16615
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-06-13 14:07:20 -0400

    Adding type prop to Icon

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -1,17 +1,27 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button } from 'a11y-components';
 import styles from './Icon.module.scss';
 
-const Icon = ({ name, small, large, spin }) => {
+const Icon = ({ large, name = 'smile', onClick, small, spin, type }) => {
   let cx = classNames.bind(styles);
   let className = cx('Icon', {
     'Icon--small': !!small,
     'Icon--large': !!large,
   });
 
-  return (
-    <FontAwesomeIcon className={className} icon={name || 'smile'} spin={spin} />
+  const icon = (
+    <FontAwesomeIcon className={className} icon={name} spin={spin} />
   );
+  if (type === 'button') {
+    return (
+      <Button className={styles.IconButton} onClick={onClick}>
+        {icon}
+      </Button>
+    );
+  } else {
+    return icon;
+  }
 };
 
 export default Icon;
