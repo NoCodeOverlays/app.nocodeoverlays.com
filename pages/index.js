9e642abcb2806ea863cb9b4734507c3caf0c7b76
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-06-14 07:41:15 -0400

    Using path aliases in index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import { Icon, Layout } from '../components';
-import styles from '../styles/Pages.module.scss';
+import { Icon, Layout } from '@components';
+import styles from '@styles/Pages.module.scss';
 
 const links = [
   {
